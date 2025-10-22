1a6f6e13d71e05698d68d5d003b6c4e67240d7c6
mikelarg <murtazkorpaz@gmail.com>
2025-10-22 17:27:28 +0300

    - deleted console.log

--- front/src/components/InputArea.tsx
+++ front/src/components/InputArea.tsx
@@ -165,7 +165,6 @@ const InputArea: React.FC<InputAreaProps> = ({ thread }) => {
         },
       } as HumanMessage;
       clear();
-      console.log(collections);
       thread?.submit(
         { messages: [newMessage], collections: collections },
         {
